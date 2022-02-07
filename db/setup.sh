901ce8d9aea183ae87ec9acfc6c6a1d999ce11e6
Joaquín Muleiro <joaquin.muleirobeltran@wearepsh.com>
2022-02-06 21:02:53 -0300

    setup and installation scripts for db vm

---
+++ db/setup.sh
@@ -0,0 +1,17 @@
+set -e
+
+# Docker installation
+apt-get -qq remove docker docker-engine docker.io containerd runc
+apt-get -qq update
+apt-get -qq install ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get -qq update
+apt-get -qq install docker-ce docker-ce-cli containerd.io
+
+# Docker Compose installation
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
